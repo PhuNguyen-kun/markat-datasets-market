0231e7f25842711bd6e627d576b09f953dc67119
Kaivu10102004 <164660261+Kaivu10102004@users.noreply.github.com>
2024-08-26 09:19:07 +0700

    first commit

---
+++ server/utils/logger.js
@@ -0,0 +1,9 @@
+const pino = require("pino");
+const pretty = require('pino-pretty');
+// Create a logging instance
+const logger = pino(pretty({
+  level: process.env.NODE_ENV === "production" ? "info" : "debug",
+  prettyPrint: process.env.NODE_ENV !== "production",
+}));
+
+module.exports.logger = logger;
